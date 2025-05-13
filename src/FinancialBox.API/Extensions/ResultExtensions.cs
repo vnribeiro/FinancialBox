8868e38ac151a8df55230a31c6b1bcf372c349d6
VnRibeiro <viniciuscostaa.ribeiro@outlook.com>
2025-05-13 02:07:47 -0300

    Refactor Match method in ResultExtensions.cs
    Updated the Match method's return type from ActionResult<ApiResponse<T>> to ActionResult for simplification. Adjusted the onSuccess and onFailure parameters to return ActionResult instead of ActionResult<ApiResponse<T>>. Removed the import for FinancialBox.API.Contracts while retaining FinancialBox.Shared.ResultObjects.

--- src/FinancialBox.API/Extensions/ResultExtensions.cs
+++ src/FinancialBox.API/Extensions/ResultExtensions.cs
@@ -1,15 +1,14 @@
-﻿using FinancialBox.API.Contracts;
-using FinancialBox.Shared.ResultObjects;
+﻿using FinancialBox.Shared.ResultObjects;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FinancialBox.API.Extensions
 {
     public static class ResultExtensions
     {
-        public static ActionResult<ApiResponse<T>> Match<T>(
+        public static ActionResult Match<T>(
             this Result<T> result,
-            Func<T, ActionResult<ApiResponse<T>>> onSuccess,
-            Func<IReadOnlyList<string>, ActionResult<ApiResponse<T>>> onFailure)
+            Func<T, ActionResult> onSuccess,
+            Func<IReadOnlyList<string>, ActionResult> onFailure)
         {
             return result.IsSuccess
                 ? onSuccess(result.Value!)
