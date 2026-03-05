564403cd66371f868382d8276ef45a4008818a31
VnRibeiro <viniciuscostaa.ribeiro@outlook.com>
2026-03-04 22:30:28 -0300

    Add Email value object unit tests and project references
    Added unit tests for the Email value object to verify valid and invalid email creation. Updated FinancialBox.UnitTests.csproj to reference Application and Domain projects for test support.

---
+++ src/FinancialBox.UnitTests/Domain/EmailTests.cs
@@ -0,0 +1,36 @@
+﻿using FinancialBox.Domain.Features.Users.Errors;
+using FinancialBox.Domain.Features.Users.ValueObjects;
+
+namespace FinancialBox.UnitTests.Domain;
+
+public class EmailTests
+{
+    [Fact]
+    public void Should_CreateSuccessfully_When_EmailIsValid()
+    {
+        // Arrange
+        const string emailAddress = "teste@gmail.com";
+
+        // Act
+        var result = Email.Create(emailAddress);
+
+        // Assert
+        Assert.True(result.IsSuccess);
+        Assert.Equal(emailAddress, result.Data.Address);
+    }
+
+    [Fact]
+    public void Should_ReturnError_When_EmailIsEmpty()
+    {
+        // Arrange
+        const string emailAddress = "";
+
+        // Act
+        var result = Email.Create(emailAddress);
+
+        // Assert
+        Assert.True(result.IsFailure);
+        Assert.NotEmpty(result.Errors);
+    }
+}
+
